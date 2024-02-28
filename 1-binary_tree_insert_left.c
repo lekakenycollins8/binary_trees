356af1ac07c24093cfb9aa48daddc09d86f66f57
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-02-28 03:07:41 -0800

    delete entire tree

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,12 +12,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_left_node;
 
-	new_left_node = binary_tree_node(parent, value);
-
 	if (parent == NULL)
 	{
 		return (NULL);
 	}
+	new_left_node = binary_tree_node(parent, value);
 	if (new_left_node == NULL)
 	{
 		return (NULL);
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,22 +1,22 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right -  inserts a node as the right-child of another node
+ * binary_tree_insert_right -  inserts node as right-child of another node
  * @parent: pointer to the node to insert the right-child in
  * @value: value to store in the new node
  *
- * Return: pointer to the created node, or NULL on failure 
+ * Return: pointer to the created node, or NULL on failure
  */
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_right_node;
 
-	new_right_node = binary_tree_node(parent, value);
 	if (parent == NULL)
 	{
 		return (NULL);
 	}
+	new_right_node = binary_tree_node(parent, value);
 	if (new_right_node == NULL)
 	{
 		return (NULL);
